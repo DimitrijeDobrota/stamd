cac465e72e0c7f84768ce019ead1ca2621ead00d
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2021-08-12 13:05:57 +0200

    Fix formatting

--- main.c
+++ main.c
@@ -21,16 +21,22 @@ void usage(char *argv0)
   exit(1);
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
   char  articledirabs[PATH_MAX + 1], outdirabs[PATH_MAX + 1], mdfile[PATH_MAX + 1], htmlfile[PATH_MAX + 1];
   int i;
 
-  for (i = 1; i < argc; i++) {
-    if (argv[i][0] != '-') {
+
+  for (i = 1; i < argc; i++)
+  {
+    if (argv[i][0] != '-')
+    {
       if (articledir)
         usage(argv[0]);
-      articledir= argv[i];
-    } else if (argv[i][1] == 'o') {
+      articledir = argv[i];
+    }
+    else if (argv[i][1] == 'o')
+    {
       if (i + 1 >= argc)
         usage(argv[0]);
       outdir = argv[++i];
@@ -45,9 +51,8 @@ int main(int argc, char* argv[]){
 
   if (!outdir)
     strcpy(outdirabs, articledirabs);
-  else
-    if (!realpath(outdir, outdirabs))
-      err(1, outdir);
+  else if (!realpath(outdir, outdirabs))
+    err(1, outdir);
 
 
   sprintf(mdfile, "%s/article.md", articledirabs);
